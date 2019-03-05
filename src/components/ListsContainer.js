bcfd03487bb04a9d6e624c4ca96df1966b9e2d6a
Drew <tke578@gmail.com>
2019-03-04 23:51:05 -0800

    Updated api end point

--- src/components/ListsContainer.js
+++ src/components/ListsContainer.js
@@ -77,7 +77,7 @@ class ListsContainer extends Component {
 		if (process.env.NODE_ENV !== 'production') {
   			return 'http://localhost:3001/api/v1/lists.json'
 		}
-		return 'https://mysterious-tundra-13244.herokuapp.com/api/v1/lists.json'
+		return 'https://dry-eyrie-80113.herokuapp.com/api/v1/lists.json'
 	}
 
 	render() {
